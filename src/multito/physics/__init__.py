c3f730a75246590fdff4e3497a0ddcb2c8a51d24
nave00 <n49241231@gmail.com>
2026-04-24 08:22:20 -0700

    Update physics __init__.py to expose loss functions

--- src/multito/physics/__init__.py
+++ src/multito/physics/__init__.py
@@ -1,1 +1,11 @@
 """Physics models and simulations for electrical machine optimization."""
+
+from .losses import dc_resistance, dc_loss, skin_depth, ac_resistance, ac_loss
+
+__all__ = [
+    "dc_resistance",
+    "dc_loss",
+    "skin_depth",
+    "ac_resistance",
+    "ac_loss",
+]
