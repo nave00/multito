2c353a19ab7cfce667b0aebe8f14840052c139e8
nave00 <n49241231@gmail.com>
2026-04-24 07:54:54 -0700

    Update topology __init__.py to import all topologies

--- src/multito/topology/__init__.py
+++ src/multito/topology/__init__.py
@@ -1,1 +1,11 @@
 """Topology definitions for different machine winding structures."""
+# Import topology classes to ensure they are registered when this package is imported
+from .solid import SolidTopology
+from .hollow import HollowTopology
+from .litz import LitzTopology
+
+__all__ = [
+    "SolidTopology",
+    "HollowTopology",
+    "LitzTopology",
+]
